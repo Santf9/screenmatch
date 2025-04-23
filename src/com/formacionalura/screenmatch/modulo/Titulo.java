9b2417adf94dc19ccd28c6231fe52679f98d858a
Santiago Ferreira <santiandresfer@hotmail.com>
2025-04-23 16:33:48 -0300

    Fix Titulo

--- src/com/formacionalura/screenmatch/modulo/Titulo.java
+++ src/com/formacionalura/screenmatch/modulo/Titulo.java
@@ -2,15 +2,14 @@ package com.formacionalura.screenmatch.modulo;
 
 public class Titulo implements Comparable<Titulo> {
     //Atributos de la Clase Título
-
     private String nombreDePelicula;
     private int fechaLanzamiento; // Modificador de acceso private
     private int duracionEnMinutos;
     private boolean estaIncluido;
     private double sumaEvaluaciones;
     private int totalDeEvaluaciones;
-    // Constructor
 
+    // Constructor
     public Titulo(String nombreDePelicula, int fechaLanzamiento) {
         this.nombreDePelicula = nombreDePelicula;
         this.fechaLanzamiento = fechaLanzamiento;
@@ -18,7 +17,6 @@ public class Titulo implements Comparable<Titulo> {
 
     // Modificador de acceso default - Solo se accede desde la misma clase o paquete
     // Encapsulamiento - Getters y Setters
-
     public String getNombreDePelicula() {
         return nombreDePelicula;
     }
@@ -54,14 +52,13 @@ public class Titulo implements Comparable<Titulo> {
         this.estaIncluido = estaIncluido;
     }
 
-    // Métodos de la clase Pelicula
-
     public void mostrarFichaTecnica() {
         System.out.println("El nombre del título: " + nombreDePelicula);
         System.out.println("La fecha de lanzamiento: " + fechaLanzamiento);
         System.out.println("Su duración en minutos: " + getDuracionEnMinutos());
         System.out.println("¿Está incluido en el plan? " + estaIncluido);
     }
+
     public void evalua(double nota) {
         sumaEvaluaciones += nota;
         totalDeEvaluaciones++;
